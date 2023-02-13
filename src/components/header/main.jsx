cda7a8f0ad35673bc60655737208ee7e1017523c
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-02-12 22:25:17 -0300

    inicio

--- src/components/header/main.jsx
+++ src/components/header/main.jsx
@@ -1,3 +1,11 @@
-export const Header = () => {
-    
-}
+export const Header = (itens, logo) => {
+    return(
+        <header>
+            <ul>
+                <li>
+                   {itens}
+                </li>
+            </ul>
+        </header>
+    );
+};
