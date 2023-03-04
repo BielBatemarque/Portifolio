4fc1f16af1da4abb9bc9192d0260bb0d535f5102
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-03-04 06:32:37 -0300

    ajustes css

--- src/components/header/ItemMenu.jsx
+++ src/components/header/ItemMenu.jsx
@@ -1,5 +1,5 @@
 export const ItemMenu = ({ children, onpressed }) => {
     return(
-        <p onClick={onpressed}> {children}</p>
+        <p onClick={onpressed} className='itemMenu'> {children}</p>
     );
 };
