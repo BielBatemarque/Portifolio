16ba6352c8cf0e13257f059106b1079313801dfa
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-03-26 20:34:58 -0300

    update

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -6,12 +6,13 @@ export const Home = () => {
     return(
        <section className="home section" id="home">
             <div className="home__container container grid">
-                <div className="home__content grid"></div>
+                <div className="home__content grid">
                 <Social />
-
-                <div className="home__img"></div>
-
+                </div>
+                <div className="home__img">
+                </div>
                 <Data/>
+
             </div>
        </section>
     );
