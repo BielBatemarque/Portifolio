598b1dc02642be75590d8ade4d20f560a8d4a775
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-03-25 23:58:09 -0300

    corpo

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,14 +1,16 @@
 export const Social = () => {
     return(
         <div className="home__social">
-           <a href="" className="home__social-icon" target="_blank">
+           <a href="https://www.instagram.com/bielbatemarque/" className="home__social-icon" target="_blank">
              <i className="uil uil-instagram"></i>
            </a>
-           <a href="" className="home__social-icon" target="_blank">
-                <i className="uil uil-dribble"></i>
-            </a>  
-           <a href="" className="home__social-icon" target="_blank">
+
+           <a href="https://github.com/BielBatemarque" className="home__social-icon" target="_blank">
                 <i className="uil uil-github-alt"></i>
+            </a> 
+
+            <a href="https://github.com/BielBatemarque" className="home__social-icon" target="_blank">
+                <i className="uil uil-linkedin-alt"></i>
             </a> 
         </div>
     );
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,10 +1,17 @@
-import './Home.css'
+import './Home.css';
+import { Social } from './Social';
+import { Data } from './Data';
 
 export const Home = () => {
     return(
        <section className="home section" id="home">
             <div className="home__container container grid">
                 <div className="home__content grid"></div>
+                <Social />
+
+                <div className="home__img"></div>
+
+                <Data/>
             </div>
        </section>
     );
