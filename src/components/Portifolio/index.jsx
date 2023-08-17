5b0251372f5287a7b0f32824f80c595ae18f72ae
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-17 00:51:13 -0300

    salvando as imangens em variaveis

--- src/components/Portifolio/index.jsx
+++ src/components/Portifolio/index.jsx
@@ -1,5 +1,7 @@
 import './Componente.css';
 import Port from '../../assets/port.png';
+import reactSkils from '../../assets/reactSkills.png';
+import conveniencia from '../../assets/conveniencia.png';
 import { useState } from 'react';
 
 export const Componente = () => {
@@ -9,6 +11,9 @@ export const Componente = () => {
     const [text2, setText2] = useState('');
     const [flag3, setFlag3] = useState(false);
     const [text3, setText3] = useState('');
+    const [flag4, setFlag4] = useState(false);
+    const [text4, setText4] = useState('');
+
 
 
     return(
@@ -42,11 +47,27 @@ export const Componente = () => {
                         <h4 className="descricaoBlobo" >Portifólio Pessoal</h4>
                     </div>
 
-                    <div className="bloco2">
-                        <div className="conteudoBloco2">
-                            <h4>Em Desenvolvimento</h4>
+                    <div className="bloco1"> 
+                        <div className="conteudoBloco" onMouseEnter={() => setFlag1(true)} onMouseLeave={() => setFlag1(false)}>
+                            { flag1 === true ?
+                                <div className='conteudoSobre'>
+                                    <div className='tecnologias'>
+                                    <h4 className='title'>Portifólio Pessoal</h4>
+                                        <h4 className='desc'>Técnologias Utilizadas</h4>
+                                        <ul className='teclist'>
+                                        <i className="uil uil-react icon icon-react" onMouseEnter={() => setText('React.Js')}></i>
+                                        <i className="uil uil-java-script icon icon-js" onMouseEnter={() => setText('JavaScript')}></i>
+                                        <i className="uil uil-css3-simple icon icon-css" onMouseEnter={() => setText('Css')}></i>
+                                        </ul>
+                                    </div>
+                                        <h3 className='descTEc'>{text}</h3>
+                                </div>    
+                                :
+                                <img src={Port} alt="Portifólio Pes'soa" className='imgb1'/>                 
+
+                        }
                         </div>
-                        <h4 className="descricaoBlobo">Em Breve</h4>
+                        <h4 className="descricaoBlobo" >Portifólio Pessoal</h4>
                     </div>
                 </div>
             </div>
