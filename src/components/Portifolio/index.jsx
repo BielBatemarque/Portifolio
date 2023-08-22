2b2d8836c17ff9fcb2b0a9deb04f39dbf0341e9c
BielBatemarque <gabrielb.sdeoliveira12@gmail.com>
2023-08-22 00:26:44 -0300

    ajuste no projeto

--- src/components/Portifolio/index.jsx
+++ src/components/Portifolio/index.jsx
@@ -94,25 +94,11 @@ export const Componente = () => {
                     </div>
 
                     <div className="bloco4"> 
-                        <div className="conteudoBloco" onMouseEnter={() => setFlag4(true)} onMouseLeave={() => setFlag4(false)}>
-                            { flag4 === true ?
-                                <div className='conteudoSobre'>
-                                    <div className='tecnologias'>
-                                    <h4 className='title'>Portifólio Pessoal</h4>
-                                        <h4 className='desc'>Técnologias Utilizadas</h4>
-                                        <ul className='teclist'>
-                                        <i className="uil uil-react icon icon-react" onMouseEnter={() => setText('React.Js')}></i>
-                                        <i className="uil uil-java-script icon icon-js" onMouseEnter={() => setText('JavaScript')}></i>
-                                        <i className="uil uil-css3-simple icon icon-css" onMouseEnter={() => setText('Css')}></i>
-                                        </ul>
-                                    </div>
-                                        <h3 className='descTEc'>{text}</h3>
-                                </div>    
-                                :
-                                <img src={Port} alt="Portifólio Pes'soa" className='imgb1'/>                 
-                        }
+                        <div className="conteudoBloco2">
+                            <h4>Em Desenvolvimento</h4>
                         </div>
-                        <h4 className="descricaoBlobo" >Portifólio Pessoal</h4>
+                     <h4 className="descricaoBlobo">Em Breve</h4>
+            
                     </div>
                 </div>
             </div>
